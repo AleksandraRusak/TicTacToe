e8ce18f7d474222990bf7c20b3196802894ee774
AleksandraRusak <rusakaleksandra94@gmail.com>
2023-09-11 19:42:49 +0200

    start project

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Aleksandra Rusak on 2023-09-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
